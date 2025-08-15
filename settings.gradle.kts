42f1106a3a0eae2cb9e0b6ec32d8d49d912a80a9
idem77 <karansingh7395082@gmail.com>
2025-08-15 13:00:18 +0530

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,17 @@
 rootProject.name = "TapnexEmpire"
+
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
