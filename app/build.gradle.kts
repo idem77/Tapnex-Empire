0d165b54effa68eb8101d49fa90d0f47dec3aaa3
idem77 <karansingh7395082@gmail.com>
2025-08-19 16:53:54 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,15 +1,34 @@
-// build.gradle.kts (Project-level)
+plugins {
+    id("com.android.application") version "8.2.1"
+    kotlin("android") version "1.9.0"
+}
 
-buildscript {
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.3.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
+android {
+    namespace = "com.tapnex.empire"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.tapnex.empire"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
     }
 }
 
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
+dependencies {
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
 }
