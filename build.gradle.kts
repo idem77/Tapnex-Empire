028971b8720976604c0bea62d32476c871ed6289
idem77 <karansingh7395082@gmail.com>
2025-08-22 16:11:18 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,15 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    // Android Gradle plugin
     id("com.android.application") version "8.2.1" apply false
-    // Kotlin plugin
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+}
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
