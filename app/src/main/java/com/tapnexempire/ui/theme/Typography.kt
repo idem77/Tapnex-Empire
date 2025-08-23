f8a6ceac011953ac2b10045726a145398a4a0626
idem77 <karansingh7395082@gmail.com>
2025-08-23 15:46:59 +0530

    Update Typography.kt

---
+++ app/src/main/java/com/tapnexempire/ui/theme/Typography.kt
@@ -0,0 +1,21 @@
+package com.tapnexempire.ui.theme
+
+import androidx.compose.material.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.unit.sp
+
+val Typography = Typography(
+    body1 = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontSize = 16.sp
+    ),
+    button = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontSize = 14.sp
+    ),
+    caption = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontSize = 12.sp
+    )
+)
