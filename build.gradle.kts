063b99a009e92d12651674b7d0646d3b4b9b907b
idem77 <karansingh7395082@gmail.com>
2025-08-25 08:52:52 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,13 +3,6 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
 
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
 }
