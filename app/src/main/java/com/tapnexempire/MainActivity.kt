e3fc3c8bf6dd1770a6c961bcaa6da66bce628987
idem77 <karansingh7395082@gmail.com>
2025-08-25 08:42:20 +0530

    Update MainActivity.kt

--- app/src/main/java/com/tapnexempire/MainActivity.kt
+++ app/src/main/java/com/tapnexempire/MainActivity.kt
@@ -4,9 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.MaterialTheme
 import androidx.compose.ui.Modifier
 import com.tapnexempire.ui.theme.TapnexEmpireTheme
 
@@ -19,7 +19,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Text("Hello Tapnex Empire 👑🚀")
+                    Text("Hello Tapnex Empire 👑🚀", style = MaterialTheme.typography.h6)
                 }
             }
         }
