dba956e368c678559e8d68f1eda3cf6046065ebd
idem77 <karansingh7395082@gmail.com>
2025-08-26 16:11:57 +0530

    Update MainActivity.kt

--- app/src/main/java/com/tapnexempire/MainActivity.kt
+++ app/src/main/java/com/tapnexempire/MainActivity.kt
@@ -3,15 +3,25 @@ package com.tapnexempire
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.*
+import com.tapnexempire.screen.HomeScreen
+import com.tapnexempire.screen.SplashScreen
 import com.tapnexempire.ui.theme.TapnexEmpireTheme
-import com.tapnexempire.screen.GameScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             TapnexEmpireTheme {
-                GameScreen() // 👈 default screen (later we add navigation)
+                var showSplash by remember { mutableStateOf(true) }
+
+                if (showSplash) {
+                    SplashScreen {
+                        showSplash = false
+                    }
+                } else {
+                    HomeScreen()
+                }
             }
         }
     }
