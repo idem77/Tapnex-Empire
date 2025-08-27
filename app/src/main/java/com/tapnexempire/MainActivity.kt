29cfb4ed7fad448808e851840733a85f94148c9b
idem77 <karansingh7395082@gmail.com>
2025-08-27 19:38:52 +0530

    Update MainActivity.kt

--- app/src/main/java/com/tapnexempire/MainActivity.kt
+++ app/src/main/java/com/tapnexempire/MainActivity.kt
@@ -3,9 +3,7 @@ package com.tapnexempire
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.*
-import com.tapnexempire.screen.HomeScreen
-import com.tapnexempire.screen.SplashScreen
+import com.tapnexempire.navigation.AppNavigation
 import com.tapnexempire.ui.theme.TapnexEmpireTheme
 
 class MainActivity : ComponentActivity() {
@@ -13,15 +11,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             TapnexEmpireTheme {
-                var showSplash by remember { mutableStateOf(true) }
-
-                if (showSplash) {
-                    SplashScreen {
-                        showSplash = false
-                    }
-                } else {
-                    HomeScreen()
-                }
+                AppNavigation()
             }
         }
     }
