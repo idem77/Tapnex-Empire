c2f524c101f5d275ad12cad83ec942cd8f03117a
idem77 <karansingh7395082@gmail.com>
2025-09-01 21:19:33 +0530

    Create NavGraph.kt

---
+++ app/src/main/java/com/tapnexempire/navigation/NavGraph.kt
@@ -0,0 +1,19 @@
+package com.tapnexempire
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.tapnexempire.screens.home.HomeScreen
+import com.tapnexempire.ui.splash.SplashScreen
+
+@Composable
+fun AppNavGraph(navController: NavHostController) {
+    NavHost(
+        navController = navController,
+        startDestination = "splash"
+    ) {
+        composable("splash") { SplashScreen(navController) }
+        composable("home") { HomeScreen() }
+    }
+}
