cda2b20a415b24851bb3d9b2ac263d606e096123
idem77 <karansingh7395082@gmail.com>
2025-09-02 19:59:22 +0530

    Update MainActivity.kt

--- app/src/main/java/com/tapnexempire/MainActivity.kt
+++ app/src/main/java/com/tapnexempire/MainActivity.kt
@@ -4,16 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.navigation.compose.rememberNavController
-import com.tapnexempire.navigation.AppNavGraph
+import com.google.firebase.FirebaseApp
+import com.tapnexempire.navigation.NavGraph
 import com.tapnexempire.ui.theme.TapnexEmpireTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        FirebaseApp.initializeApp(this) // if using Firebase
         setContent {
             TapnexEmpireTheme {
                 val navController = rememberNavController()
-                AppNavGraph(navController = navController)
+                NavGraph(navController) // pass navController
             }
         }
     }
