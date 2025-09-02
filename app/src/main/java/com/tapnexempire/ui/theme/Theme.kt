9c0a6d783900da6cc8023b92cef53db6547713f0
idem77 <karansingh7395082@gmail.com>
2025-09-02 19:55:06 +0530

    Update Theme.kt

--- app/src/main/java/com/tapnexempire/ui/theme/Theme.kt
+++ app/src/main/java/com/tapnexempire/ui/theme/Theme.kt
@@ -1,43 +1,22 @@
 package com.tapnexempire.ui.theme
 
-import android.app.Activity
-import android.os.Build
-import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.WindowCompat
 
 private val LightColors = lightColorScheme(
-    primary = Purple500,
-    secondary = Teal200
-)
-
-private val DarkColors = darkColorScheme(
-    primary = Purple200,
-    secondary = Teal200
+    primary = Purple80,
+    secondary = Gold,
+    background = Black,
+    surface = Black,
+    onPrimary = White,
+    onSecondary = White,
 )
 
 @Composable
-fun TapnexEmpireTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable () -> Unit
-) {
-    val colors = if (darkTheme) DarkColors else LightColors
-
-    val view = LocalView.current
-    if (!view.isInEditMode) {
-        SideEffect {
-            val window = (view.context as Activity).window
-            window.statusBarColor = colors.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
-        }
-    }
-
+fun TapnexEmpireTheme(content: @Composable () -> Unit) {
     MaterialTheme(
-        colorScheme = colors,
+        colorScheme = LightColors,
         typography = Typography,
         content = content
     )
