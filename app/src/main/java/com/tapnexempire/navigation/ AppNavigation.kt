b1c012f8dc9cfa427b45fd9e17bb9f65f854860b
idem77 <karansingh7395082@gmail.com>
2025-09-02 21:12:33 +0530

    Update  AppNavigation.kt

--- app/src/main/java/com/tapnexempire/navigation/ AppNavigation.kt
+++ app/src/main/java/com/tapnexempire/navigation/ AppNavigation.kt
@@ -1,10 +1,37 @@
 package com.tapnexempire.navigation
 
 import androidx.compose.runtime.Composable
-import androidx.navigation.compose.rememberNavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.tapnexempire.screen.AuthScreen
+import com.tapnexempire.screen.HomeScreen
+import com.tapnexempire.screen.HelpScreen
+import com.tapnexempire.screen.PlaceholderScreen
 
 @Composable
-fun AppNavigation() {
-    val navController = rememberNavController()
-    NavGraph(navController = navController)
+fun AppNavGraph(navController: NavHostController) {
+    NavHost(
+        navController = navController,
+        startDestination = "auth"
+    ) {
+        composable("auth") {
+            AuthScreen(navController)
+        }
+        composable("home") {
+            HomeScreen(navController)
+        }
+        composable("help") {
+            HelpScreen(navController)
+        }
+        composable("profile") {
+            PlaceholderScreen("Profile Screen")
+        }
+        composable("wallet") {
+            PlaceholderScreen("Wallet Screen")
+        }
+        composable("redeem") {
+            PlaceholderScreen("Redeem Screen")
+        }
+    }
 }
