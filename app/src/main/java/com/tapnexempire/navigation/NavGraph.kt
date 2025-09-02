c50990e737372268d875b1739dd77131662ee469
idem77 <karansingh7395082@gmail.com>
2025-09-02 05:59:13 +0530

    Update NavGraph.kt

--- app/src/main/java/com/tapnexempire/navigation/NavGraph.kt
+++ app/src/main/java/com/tapnexempire/navigation/NavGraph.kt
@@ -1,19 +1,52 @@
-package com.tapnexempire
+package com.tapnexempire.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import com.tapnexempire.screens.home.HomeScreen
-import com.tapnexempire.ui.splash.SplashScreen
+import com.tapnexempire.screen.*
 
 @Composable
-fun AppNavGraph(navController: NavHostController) {
+fun NavGraph(navController: NavHostController) {
     NavHost(
         navController = navController,
         startDestination = "splash"
     ) {
-        composable("splash") { SplashScreen(navController) }
-        composable("home") { HomeScreen() }
+        composable("splash") {
+            PlaceholderScreen("Splash")
+        }
+        composable("home") {
+            HomeScreen(
+                onGameClick = { navController.navigate("games") },
+                onWalletClick = { navController.navigate("wallet") },
+                onRedeemClick = { navController.navigate("redeem") },
+                onHelpClick = { navController.navigate("help") },
+                onProfileClick = { navController.navigate("profile") }
+            )
+        }
+        composable("games") {
+            GameScreen()
+        }
+        composable("wallet") {
+            PlaceholderScreen("Wallet")
+        }
+        composable("redeem") {
+            PlaceholderScreen("Redeem")
+        }
+        composable("help") {
+            HelpScreen(
+                onFaqClick = { navController.navigate("faq") },
+                onContactClick = { navController.navigate("contact") }
+            )
+        }
+        composable("profile") {
+            PlaceholderScreen("Profile")
+        }
+        composable("faq") {
+            PlaceholderScreen("FAQ")
+        }
+        composable("contact") {
+            PlaceholderScreen("Contact Us")
+        }
     }
 }
