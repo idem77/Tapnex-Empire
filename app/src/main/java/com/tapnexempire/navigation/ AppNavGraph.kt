2ae087225fb5408eed0ca1856819ab7c43a02b35
idem77 <karansingh7395082@gmail.com>
2025-09-04 18:56:53 +0530

    Update  AppNavGraph.kt

--- app/src/main/java/com/tapnexempire/navigation/ AppNavGraph.kt
+++ app/src/main/java/com/tapnexempire/navigation/ AppNavGraph.kt
@@ -4,7 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import com.tapnexempire.screens.*
+import com.tapnexempire.screen.*
 
 @Composable
 fun AppNavGraph(navController: NavHostController) {
