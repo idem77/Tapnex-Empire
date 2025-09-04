521f925d9b5dabad9b6ed681226ef08694b082e5
idem77 <karansingh7395082@gmail.com>
2025-09-04 18:31:26 +0530

    Update  AppNavGraph.kt

--- app/src/main/java/com/tapnexempire/navigation/ AppNavGraph.kt
+++ app/src/main/java/com/tapnexempire/navigation/ AppNavGraph.kt
@@ -4,27 +4,37 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import com.tapnexempire.screens.HomeScreen
-import com.tapnexempire.screens.WalletScreen
-import com.tapnexempire.screens.RedeemScreen
-import com.tapnexempire.screens.TaskScreen
-
-sealed class Screen(val route: String) {
-    object Home : Screen("home")
-    object Wallet : Screen("wallet")
-    object Redeem : Screen("redeem")
-    object Task : Screen("task")
-}
+import com.tapnexempire.screens.*
 
 @Composable
 fun AppNavGraph(navController: NavHostController) {
     NavHost(
         navController = navController,
-        startDestination = Screen.Home.route
+        startDestination = "splash"
     ) {
-        composable(Screen.Home.route) { HomeScreen(navController) }
-        composable(Screen.Wallet.route) { WalletScreen(navController) }
-        composable(Screen.Redeem.route) { RedeemScreen(navController) }
-        composable(Screen.Task.route) { TaskScreen(navController) }
+        composable("splash") {
+            SplashScreen(navController)
+        }
+        composable("home") {
+            HomeScreen(navController)
+        }
+        composable("wallet") {
+            WalletScreen(navController)
+        }
+        composable("redeem") {
+            RedeemScreen(navController)
+        }
+        composable("tasks") {
+            TaskScreen(navController)
+        }
+        composable("profile") {
+            ProfileScreen(navController)
+        }
+        composable("offers") {
+            OffersScreen(navController)
+        }
+        composable("help") {
+            HelpScreen(navController)
+        }
     }
 }
