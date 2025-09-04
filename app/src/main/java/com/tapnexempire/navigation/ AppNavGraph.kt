375371ef931814d8a3810a641f46806f1b5f2c1d
idem77 <karansingh7395082@gmail.com>
2025-09-04 17:00:10 +0530

    Update  AppNavGraph.kt

--- app/src/main/java/com/tapnexempire/navigation/ AppNavGraph.kt
+++ app/src/main/java/com/tapnexempire/navigation/ AppNavGraph.kt
@@ -4,34 +4,27 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import com.tapnexempire.screen.AuthScreen
-import com.tapnexempire.screen.HomeScreen
-import com.tapnexempire.screen.HelpScreen
-import com.tapnexempire.screen.PlaceholderScreen
+import com.tapnexempire.screens.HomeScreen
+import com.tapnexempire.screens.WalletScreen
+import com.tapnexempire.screens.RedeemScreen
+import com.tapnexempire.screens.TaskScreen
+
+sealed class Screen(val route: String) {
+    object Home : Screen("home")
+    object Wallet : Screen("wallet")
+    object Redeem : Screen("redeem")
+    object Task : Screen("task")
+}
 
 @Composable
 fun AppNavGraph(navController: NavHostController) {
     NavHost(
         navController = navController,
-        startDestination = "auth"
+        startDestination = Screen.Home.route
     ) {
-        composable("auth") {
-            AuthScreen(navController)
-        }
-        composable("home") {
-            HomeScreen(navController)
-        }
-        composable("help") {
-            HelpScreen(navController)
-        }
-        composable("profile") {
-            PlaceholderScreen("Profile Screen")
-        }
-        composable("wallet") {
-            PlaceholderScreen("Wallet Screen")
-        }
-        composable("redeem") {
-            PlaceholderScreen("Redeem Screen")
-        }
+        composable(Screen.Home.route) { HomeScreen(navController) }
+        composable(Screen.Wallet.route) { WalletScreen(navController) }
+        composable(Screen.Redeem.route) { RedeemScreen(navController) }
+        composable(Screen.Task.route) { TaskScreen(navController) }
     }
 }
