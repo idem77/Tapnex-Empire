9877f69340bef6bfa9da1d430e7e1fddd2f38024
idem77 <karansingh7395082@gmail.com>
2025-09-04 18:24:29 +0530

    Update Color.kt

--- app/src/main/java/com/tapnexempire/ui/theme/Color.kt
+++ app/src/main/java/com/tapnexempire/ui/theme/Color.kt
@@ -2,11 +2,10 @@ package com.tapnexempire.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
-val NeonBlue = Color(0xFF00FFFF) // your bright cyan-blue
-val Black = Color(0xFF000000)
-val White = Color(0xFFFFFFFF)
-val Gold = Color(0xFFFFD700)
+// 🎨 Tapnex Empire Heart Palette
+val RoyalBlue = Color(0xFF0D47A1)     // Primary
+val Gold = Color(0xFFFFD700)          // Wealth
+val NeonGreen = Color(0xFF00E676)     // Energy
+val MidnightBlack = Color(0xFF0A0A0A) // Background
+val PureWhite = Color(0xFFFFFFFF)     // Contrast
+val LightGray = Color(0xFFBDBDBD)     // Secondary text
