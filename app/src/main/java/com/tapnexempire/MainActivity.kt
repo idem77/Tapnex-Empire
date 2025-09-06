79f4d36014601ce7178978de6f1a0f37974e49d8
idem77 <karansingh7395082@gmail.com>
2025-09-06 10:20:21 +0530

    Update MainActivity.kt

--- app/src/main/java/com/tapnexempire/MainActivity.kt
+++ app/src/main/java/com/tapnexempire/MainActivity.kt
@@ -3,23 +3,32 @@ package com.tapnexempire
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.Surface
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import com.tapnexempire.navigation.AppNavGraph
+import com.tapnexempire.components.BottomNavBar
 import com.tapnexempire.ui.theme.TapnexEmpireTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            TapnexEmpireTheme {
-                Surface(
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    val navController = rememberNavController()
-                    AppNavGraph(navController = navController)
-                }
+        setContent { TapnexEmpireApp() }
+    }
+}
+
+@Composable
+fun TapnexEmpireApp() {
+    val navController = rememberNavController()
+
+    TapnexEmpireTheme {
+        Scaffold(
+            bottomBar = { BottomNavBar(navController) }
+        ) { innerPadding ->
+            Box(modifier = Modifier.padding(innerPadding)) {
+                AppNavGraph(navController = navController)
             }
         }
     }
