e2742bfdcb60ad5136c73148922025e9266993b7
idem77 <karansingh7395082@gmail.com>
2025-09-06 10:18:34 +0530

    Update  AppNavGraph.kt

--- app/src/main/java/com/tapnexempire/navigation/ AppNavGraph.kt
+++ app/src/main/java/com/tapnexempire/navigation/ AppNavGraph.kt
@@ -4,37 +4,28 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import com.tapnexempire.screen.*
+import com.tapnexempire.screens.*
 
 @Composable
 fun AppNavGraph(navController: NavHostController) {
-    NavHost(
-        navController = navController,
-        startDestination = "splash"
-    ) {
-        composable("splash") {
-            SplashScreen(navController)
-        }
-        composable("home") {
-            HomeScreen(navController)
-        }
-        composable("wallet") {
-            WalletScreen(navController)
-        }
-        composable("redeem") {
-            RedeemScreen(navController)
-        }
-        composable("tasks") {
-            TaskScreen(navController)
-        }
-        composable("profile") {
-            ProfileScreen(navController)
-        }
-        composable("offers") {
-            OffersScreen(navController)
-        }
-        composable("help") {
-            HelpScreen(navController)
-        }
+    NavHost(navController = navController, startDestination = "splash") {
+
+        composable("splash") { SplashScreen(onContinue = { navController.navigate("home") }) }
+
+        composable("home") { HomeScreen(
+            onWalletClick = { navController.navigate("wallet") },
+            onTaskClick = { navController.navigate("task") },
+            onGameClick = { navController.navigate("game") },
+            onRedeemClick = { navController.navigate("redeem") },
+            onTournamentClick = { navController.navigate("tournament") },
+            onProfileClick = { navController.navigate("profile") }
+        )}
+
+        composable("wallet") { WalletScreen() }
+        composable("task") { TaskScreen() }
+        composable("game") { GameScreen() }
+        composable("redeem") { RedeemScreen() }
+        composable("tournament") { TournamentScreen() }
+        composable("profile") { ProfileScreen() }
     }
 }
