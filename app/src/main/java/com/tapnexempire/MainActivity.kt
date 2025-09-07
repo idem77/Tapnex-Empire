8fd6030034f530f65653b945265adfefcc3cfd1b
idem77 <karansingh7395082@gmail.com>
2025-09-07 11:28:37 +0530

    Update MainActivity.kt

--- app/src/main/java/com/tapnexempire/MainActivity.kt
+++ app/src/main/java/com/tapnexempire/MainActivity.kt
@@ -3,7 +3,8 @@ package com.tapnexempire
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -15,7 +16,9 @@ import com.tapnexempire.ui.theme.TapnexEmpireTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { TapnexEmpireApp() }
+        setContent {
+            TapnexEmpireApp()
+        }
     }
 }
 
@@ -25,7 +28,7 @@ fun TapnexEmpireApp() {
 
     TapnexEmpireTheme {
         Scaffold(
-            bottomBar = { BottomNavBar(navController) }
+            bottomBar = { BottomNavBar(navController = navController) }
         ) { innerPadding ->
             Box(modifier = Modifier.padding(innerPadding)) {
                 AppNavGraph(navController = navController)
