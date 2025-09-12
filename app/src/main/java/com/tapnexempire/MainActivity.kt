04bbcf3813cb771cf14327677e89fa666550ad6f
idem77 <karansingh7395082@gmail.com>
2025-09-12 20:33:38 +0530

    Update MainActivity.kt

--- app/src/main/java/com/tapnexempire/MainActivity.kt
+++ app/src/main/java/com/tapnexempire/MainActivity.kt
@@ -3,21 +3,17 @@ package com.tapnexempire
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.Surface
-import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.rememberNavController
+import com.tapnexempire.navigation.AppNavGraph
 import com.tapnexempire.ui.theme.TapnexEmpireTheme
-import com.tapnexempire.ui.navigation.AppNavHost
-import com.tapnexempire.ui.viewmodel.MainViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             TapnexEmpireTheme {
-                Surface {
-                    val vm: MainViewModel = viewModel()
-                    AppNavHost(viewModel = vm)
-                }
+                val navController = rememberNavController()
+                AppNavGraph(navController)
             }
         }
     }
