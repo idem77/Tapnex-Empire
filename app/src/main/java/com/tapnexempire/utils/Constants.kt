f4d596a9c09d9a5b8f20575aa61d82cbbd0f2e7a
idem77 <karansingh7395082@gmail.com>
2025-09-12 20:39:26 +0530

    Create Constants.kt

---
+++ app/src/main/java/com/tapnexempire/utils/Constants.kt
@@ -0,0 +1,12 @@
+package com.tapnexempire.utils
+
+object Constants {
+    const val PACKAGE_NAME = "com.tapnexempire"
+    const val COINS_PER_RUPEE = 100  // 1 ₹ = 100 coins -> 1000 coins = ₹10
+    const val MIN_WITHDRAW_RUPEES = 10
+    const val MAX_WITHDRAW_RUPEES = 500
+    const val AD_COINS_PER_VIEW = 25
+    const val MAX_ADS_PER_DAY = 5
+    const val TOURNAMENT_TAX_PERCENT = 20
+    const val WITHDRAW_PROCESS_TIME_HOURS = 48
+}
