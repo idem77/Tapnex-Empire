f3d5b328cd671561078d6fb9cb2ea551d711032a
idem77 <karansingh7395082@gmail.com>
2025-09-16 10:09:08 +0530

    Update SplashScreen.kt

--- app/src/main/java/com/tapnexempire/ui/splash/SplashScreen.kt
+++ app/src/main/java/com/tapnexempire/ui/splash/SplashScreen.kt
@@ -13,7 +13,7 @@ import androidx.compose.ui.draw.scale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import com.tapnexempire.R
+
 import com.tapnexempire.navigation.Screen
 import kotlinx.coroutines.delay
 
