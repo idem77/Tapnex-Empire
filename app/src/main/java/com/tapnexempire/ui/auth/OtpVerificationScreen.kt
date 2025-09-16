5164ec253ffca655159f2712cf066067f483219f
idem77 <karansingh7395082@gmail.com>
2025-09-16 10:08:00 +0530

    Update OtpVerificationScreen.kt

--- app/src/main/java/com/tapnexempire/ui/auth/OtpVerificationScreen.kt
+++ app/src/main/java/com/tapnexempire/ui/auth/OtpVerificationScreen.kt
@@ -9,7 +9,7 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.tapnexempire.navigation.Screen
-
+import androidx.compose.ui.text.input.KeyboardOptions
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun OtpVerificationScreen(navController: NavController) {
