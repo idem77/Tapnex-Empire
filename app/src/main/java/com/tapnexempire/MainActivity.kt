48cfbe3fb7b40396a544ff9c6c34ef108311a76f
idem77 <karansingh7395082@gmail.com>
2025-09-17 18:57:56 +0530

    Update MainActivity.kt

--- app/src/main/java/com/tapnexempire/MainActivity.kt
+++ app/src/main/java/com/tapnexempire/MainActivity.kt
@@ -1,19 +1,20 @@
+@file:OptIn(ExperimentalMaterial3Api::class) // Queen’s magic for no warnings 😏💖
+
 package com.tapnexempire
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.navigation.compose.rememberNavController
-import com.tapnexempire.navigation.AppNavGraph
 import com.tapnexempire.ui.theme.TapnexEmpireTheme
+import com.tapnexempire.navigation.AppNavGraph
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             TapnexEmpireTheme {
-                val navController = rememberNavController()
-                AppNavGraph(navController)
+                // App Navigation Graph
+                AppNavGraph()
             }
         }
     }
