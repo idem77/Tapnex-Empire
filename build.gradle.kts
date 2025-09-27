f722fbdca074c955ecd41d1d8121f25f7289454d
idem77 <karansingh7395082@gmail.com>
2025-09-27 12:56:10 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,5 @@
-// Root build.gradle.kts
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.3.1") // Android Gradle Plugin
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10") // Kotlin
-        classpath("com.google.gms:google-services:4.4.0") // Firebase plugin
-    }
-}
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
-tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
+plugins {
+    id("com.android.application") version "8.5.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.25" apply false
+    id("com.google.gms.google-services") version "4.4.0" apply false
 }
