d7618dba0734aa1c81855a5b7902866c26aa3ffe
idem77 <karansingh7395082@gmail.com>
2025-09-28 13:32:52 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,25 @@
+// TapnexEmpire/build.gradle.kts
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.3.0")
+        classpath("com.google.gms:google-services:4.4.0") // For Firebase
+    }
+}
+
 plugins {
-    id("com.android.application") version "8.5.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.25" apply false
-    id("com.google.gms.google-services") version "4.4.0" apply false
+    // Optional: Kotlin plugin
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
