09ed91b1f931365903abf9b9c551a09bcf8f7e0c
idem77 <karansingh7395082@gmail.com>
2025-09-30 14:26:29 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
-// TapnexEmpire/build.gradle.kts
+// TapnexEmpire/build.gradle.kts (Top-level)
 
 buildscript {
     repositories {
@@ -7,10 +7,12 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:8.3.0")
-        classpath("com.google.gms:google-services:4.4.0") // For Firebase
+        classpath("com.google.gms:google-services:4.4.0") // Firebase
     }
 }
 
 plugins {
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    kotlin("android") version "1.9.25" apply false
+    id("com.android.application") version "8.3.0" apply false
+    id("com.google.gms.google-services") version "4.4.0" apply false
 }
