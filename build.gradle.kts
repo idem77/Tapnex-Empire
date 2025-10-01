b5773dd38a863ca0c210fdaaddd17105f91b15f0
idem77 <karansingh7395082@gmail.com>
2025-10-01 07:16:37 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ buildscript {
 }
 
 plugins {
-    kotlin("android") version "1.9.25" apply false
     id("com.android.application") version "8.3.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.20" apply false
     id("com.google.gms.google-services") version "4.4.0" apply false
 }
