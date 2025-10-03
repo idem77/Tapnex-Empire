070fed2b9a2373b9b3a7b5fa37524ec4e239b4d3
idem77 <karansingh7395082@gmail.com>
2025-10-03 17:19:25 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application")
     kotlin("android")
-    id("com.google.gms.google-services")
+    id("com.google.gms.google-services") // Firebase
 }
 
 android {
@@ -32,7 +32,7 @@ android {
     }
 
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.15"
+        kotlinCompilerExtensionVersion = "1.5.15" // Compose compiler
     }
 
     kotlinOptions {
@@ -50,7 +50,7 @@ dependencies {
     // Jetpack Compose
     implementation("androidx.activity:activity-compose:1.9.0")
     implementation("androidx.compose.ui:ui:1.5.15")
-    implementation("androidx.compose.material3:material3:1.3.1")    // lastest stable 
+    implementation("androidx.compose.material3:material3:1.1.1")
     implementation("androidx.compose.ui:ui-tooling-preview:1.5.15")
     debugImplementation("androidx.compose.ui:ui-tooling:1.5.15")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
@@ -62,7 +62,7 @@ dependencies {
     implementation("com.google.firebase:firebase-auth-ktx")
     implementation("com.google.firebase:firebase-firestore-ktx")
 
-    // Other
+    // Core
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
-}    
+}
