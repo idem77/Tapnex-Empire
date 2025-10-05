611ac5ec53d9a3efe88300286e7fef456c16d3b3
idem77 <karansingh7395082@gmail.com>
2025-10-05 16:49:33 +0530

    Update Transaction.kt

--- app/src/main/java/com/tapnexempire/models/Transaction.kt
+++ app/src/main/java/com/tapnexempire/models/Transaction.kt
@@ -2,7 +2,7 @@ package com.tapnexempire.models
 
 data class Transaction(
     val id: String,
-    val amount: Double,
-    val type: String, // "credit" or "debit"
-    val date: String
+    val date: String,
+    val amount: Int,
+    val type: String // Deposit / Withdraw
 )
