6ce2b0dd123ea019ea81a4ffd5febd528ffffba2
idem77 <karansingh7395082@gmail.com>
2025-10-05 12:27:45 +0530

    Update Task.kt

--- app/src/main/java/com/tapnexempire/models/Task.kt
+++ app/src/main/java/com/tapnexempire/models/Task.kt
@@ -1,6 +1,8 @@
 package com.tapnexempire.models
 
 data class Task(
+    val id: String,
     val title: String,
-    val isCompleted: Boolean
+    val description: String,
+    val rewardCoins: Int
 )
