24bbeec51e3a9cfc41b2db751e549e65679f6642
idem77 <karansingh7395082@gmail.com>
2025-10-22 21:20:01 +0530

    Add FirebaseModule for FirebaseAuth dependency

---
+++ app/src/main/java/com/tapnexempire/di/FirebaseModule.kt
@@ -0,0 +1,20 @@
+// TapnexEmpire/app/src/main/java/com/tapnexempire/di/FirebaseModule.kt
+package com.tapnexempire.di
+
+import com.google.firebase.auth.FirebaseAuth
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object FirebaseModule {
+
+    @Provides
+    @Singleton
+    fun provideFirebaseAuth(): FirebaseAuth {
+        return FirebaseAuth.getInstance()
+    }
+}
