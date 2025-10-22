5574c8dea620fb394b26ecb8b4ff634aed2cd8a1
idem77 <karansingh7395082@gmail.com>
2025-10-22 20:56:42 +0530

    Create TapnexEmpireApp.kt

---
+++ app/src/main/java/com/tapnexempire/TapnexEmpireApp.kt
@@ -0,0 +1,9 @@
+// TapnexEmpire/app/src/main/java/com/tapnexempire/TapnexEmpireApp.kt
+
+package com.tapnexempire
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class TapnexEmpireApp : Application()
