6d202d4c5443f191b441b7e15b522db219a6741c
idem77 <karansingh7395082@gmail.com>
2025-10-29 11:00:57 +0530

    Update TransactionModel.kt

--- app/src/main/java/com/tapnexempire/models/TransactionModel.kt
+++ app/src/main/java/com/tapnexempire/models/TransactionModel.kt
@@ -1,6 +1,6 @@
 package com.tapnexempire.models
 
-data class Transaction(
+data class TransactionModel(
     val id: String = System.currentTimeMillis().toString(),
     val type: String,          // "Tournament Join", "Reward", "Task", etc.
     val amount: Int,
