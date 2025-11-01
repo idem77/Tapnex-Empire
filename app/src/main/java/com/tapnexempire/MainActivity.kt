2b83afb697fb90443cee72fc97f2867fcd8ecbdb
idem77 <karansingh7395082@gmail.com>
2025-11-01 07:45:47 +0530

    Update MainActivity.kt

--- app/src/main/java/com/tapnexempire/MainActivity.kt
+++ app/src/main/java/com/tapnexempire/MainActivity.kt
@@ -3,9 +3,10 @@ package com.tapnexempire
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.runtime.Composable
 import androidx.navigation.compose.rememberNavController
+import com.tapnexempire.viewmodel.AuthViewModel
+import com.tapnexempire.viewmodel.WalletViewModel
+import com.tapnexempire.viewmodel.TournamentViewModel
 import com.tapnexempire.navigation.AppNavGraph
 import com.tapnexempire.ui.theme.TapnexEmpireTheme
 
@@ -14,15 +15,19 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             TapnexEmpireTheme {
-                MyApp()
+                val navController = rememberNavController()
+
+                val authViewModel = AuthViewModel()
+                val walletViewModel = WalletViewModel()
+                val tournamentViewModel = TournamentViewModel()
+
+                AppNavGraph(
+                    navController = navController,
+                    authViewModel = authViewModel,
+                    walletViewModel = walletViewModel,
+                    tournamentViewModel = tournamentViewModel
+                )
             }
         }
     }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun MyApp() {
-    val navController = rememberNavController()
-    AppNavGraph(navController = navController)
 }
