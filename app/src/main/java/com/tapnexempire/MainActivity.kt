bfe7b31b467d36552911bf359bdc49f713134449
idem77 <karansingh7395082@gmail.com>
2025-11-02 07:12:22 +0530

    Update MainActivity.kt

--- app/src/main/java/com/tapnexempire/MainActivity.kt
+++ app/src/main/java/com/tapnexempire/MainActivity.kt
@@ -3,30 +3,26 @@ package com.tapnexempire
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Surface
+import androidx.compose.ui.graphics.Color
 import androidx.navigation.compose.rememberNavController
-import com.tapnexempire.viewmodel.AuthViewModel
-import com.tapnexempire.viewmodel.WalletViewModel
-import com.tapnexempire.viewmodel.TournamentViewModel
 import com.tapnexempire.navigation.AppNavGraph
 import com.tapnexempire.ui.theme.TapnexEmpireTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
         setContent {
             TapnexEmpireTheme {
-                val navController = rememberNavController()
-
-                val authViewModel = AuthViewModel()
-                val walletViewModel = WalletViewModel()
-                val tournamentViewModel = TournamentViewModel()
-
-                AppNavGraph(
-                    navController = navController,
-                    authViewModel = authViewModel,
-                    walletViewModel = walletViewModel,
-                    tournamentViewModel = tournamentViewModel
-                )
+                Surface(color = Color.White) {
+                    val navController = rememberNavController()
+                    AppNavGraph(navController = navController)
+                }
             }
         }
     }
