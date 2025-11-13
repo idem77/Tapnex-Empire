53fb07ea14b08d6a0eeb1a3f517fa7d791772dbd
idem77 <karansingh7395082@gmail.com>
2025-11-13 19:45:32 +0530

    Update WalletModel.kt

--- app/src/main/java/com/tapnexempire/models/WalletModel.kt
+++ app/src/main/java/com/tapnexempire/models/WalletModel.kt
@@ -1,15 +1,9 @@
 package com.tapnexempire.models
 
-data class Wallet(
-    val depositBalance: Int = 0,
-    val withdrawableBalance: Int = 0,
-    val referralRewards: Int = 0,
-    val totalCoins: Int = 0
-)
-
-data class Task(
-    val id: String,
-    val title: String,
-    val rewardCoins: Int,
-    val isCompleted: Boolean = false
-)
+data class WalletModel(
+    val depositBalance: Int = 0,          // Coins from tasks, bonuses, referrals, deposits
+    val withdrawableBalance: Int = 0,     // Only from winnings
+) {
+    val totalBalance: Int
+        get() = depositBalance + withdrawableBalance
+}
