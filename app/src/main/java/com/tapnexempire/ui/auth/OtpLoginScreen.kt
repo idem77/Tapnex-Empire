60d4d31a76492c3c6c7115d93bc2206577f4aee2
idem77 <karansingh7395082@gmail.com>
2025-11-17 12:30:16 +0530

    Update OtpLoginScreen.kt

--- app/src/main/java/com/tapnexempire/ui/auth/OtpLoginScreen.kt
+++ app/src/main/java/com/tapnexempire/ui/auth/OtpLoginScreen.kt
@@ -1,12 +1,12 @@
 package com.tapnexempire.ui.auth
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.KeyboardOptions
 import androidx.compose.ui.unit.dp
 import com.tapnexempire.viewmodel.AuthViewModel
 
