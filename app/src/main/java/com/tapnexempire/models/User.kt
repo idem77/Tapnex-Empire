827cf7827a257c7d51ff84eba8b95a84a05708c4
idem77 <karansingh7395082@gmail.com>
2025-12-21 18:43:57 +0530

    Update User.kt

--- app/src/main/java/com/tapnexempire/models/User.kt
+++ app/src/main/java/com/tapnexempire/models/User.kt
@@ -1,12 +1,7 @@
 package com.tapnexempire.models
 
-data class User(
-    val id: String,                // Unique user ID
-    val name: String,              // User's display name
-    val email: String? = null,     // Optional email
-    val coins: Double = 0.0,       // Total coins user has
-    val depositBalance: Double = 0.0, // Coins deposited (non-withdrawable)
-    val withdrawableBalance: Double = 0.0, // Coins that can be used for games/tasks
-    val referralRewards: Double = 0.0, // Coins earned from referrals
-    val joinedTournaments: List<String> = emptyList() // List of tournament IDs
+data class UserModel(
+    val uid: String,
+    val phone: String,
+    val isNewUser: Boolean = false
 )
