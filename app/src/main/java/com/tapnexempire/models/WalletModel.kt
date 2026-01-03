e65ccb7b478ae284078504b83bfca639aee515f3
idem77 <karansingh7395082@gmail.com>
2026-01-03 17:29:37 +0530

    Update WalletModel.kt

--- app/src/main/java/com/tapnexempire/models/WalletModel.kt
+++ app/src/main/java/com/tapnexempire/models/WalletModel.kt
@@ -2,9 +2,6 @@ package com.tapnexempire.models
 
 data class WalletModel(
     val depositCoins: Int = 0,
-    val winningCoins: Int = 0,
-    val transactions: List<TransactionModel> = emptyList()
-) {
-    val totalCoins: Int
-        get() = depositCoins + winningCoins
-}
+    val bonusCoins: Int = 0,
+    val withdrawableCoins: Int = 0
+)
