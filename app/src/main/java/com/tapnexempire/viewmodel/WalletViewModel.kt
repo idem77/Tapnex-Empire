5bfb4fb85382892c8b27fae2e40bded2231beefc
idem77 <karansingh7395082@gmail.com>
2026-01-05 19:00:12 +0530

    Update WalletViewModel.kt

--- app/src/main/java/com/tapnexempire/viewmodel/WalletViewModel.kt
+++ app/src/main/java/com/tapnexempire/viewmodel/WalletViewModel.kt
@@ -2,26 +2,23 @@ package com.tapnexempire.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.tapnexempire.models.WalletModel
 import com.tapnexempire.repository.WalletRepository
-import com.tapnexempire.model.Wallet
-import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
-import javax.inject.Inject
 
-@HiltViewModel
-class WalletViewModel @Inject constructor(
+class WalletViewModel(
     private val walletRepository: WalletRepository
 ) : ViewModel() {
 
-    private val _wallet = MutableStateFlow(Wallet())
-    val wallet: StateFlow<Wallet> = _wallet
+    private val _walletState = MutableStateFlow<WalletModel?>(null)
+    val walletState: StateFlow<WalletModel?> = _walletState
 
     fun loadWallet(userId: String) {
         viewModelScope.launch {
             walletRepository.createWalletIfNotExists(userId)
-            _wallet.value = walletRepository.getWallet(userId)
+            _walletState.value = walletRepository.getWallet(userId)
         }
     }
 }
