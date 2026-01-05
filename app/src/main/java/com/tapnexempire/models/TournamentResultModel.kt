b75a401b7ed0acfbf9c02997531245657576d9ef
idem77 <karansingh7395082@gmail.com>
2026-01-05 19:05:52 +0530

    Update TournamentResultModel.kt

--- app/src/main/java/com/tapnexempire/models/TournamentResultModel.kt
+++ app/src/main/java/com/tapnexempire/models/TournamentResultModel.kt
@@ -1,1 +1,10 @@
+package com.tapnexempire.models
 
+data class TournamentResultModel(
+    val tournamentId: String = "",
+    val userId: String = "",
+    val rank: Int = 0,
+    val winCoins: Int = 0,
+    val isWithdrawable: Boolean = false,
+    val createdAt: Long = System.currentTimeMillis()
+)
