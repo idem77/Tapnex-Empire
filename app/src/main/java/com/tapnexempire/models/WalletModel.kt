162698a53e39995840cba4f1c9c5f5a1bca0ecb4
idem77 <karansingh7395082@gmail.com>
2026-01-05 18:58:38 +0530

    Update WalletModel.kt

--- app/src/main/java/com/tapnexempire/models/WalletModel.kt
+++ app/src/main/java/com/tapnexempire/models/WalletModel.kt
@@ -1,7 +1,11 @@
 package com.tapnexempire.models
 
 data class WalletModel(
-    val depositCoins: Int = 0,
-    val bonusCoins: Int = 0,
-    val withdrawableCoins: Int = 0
+    val userId: String = "",
+    val depositCoins: Int = 0,        // User ke real deposit coins
+    val bonusCoins: Int = 0,          // Ads / tasks se aaye
+    val withdrawableCoins: Int = 0,   // Withdraw allowed
+    val lockedCoins: Int = 0,         // Free / survey winnings
+    val totalEarnings: Int = 0,       // Lifetime coins earned
+    val updatedAt: Long = System.currentTimeMillis()
 )
