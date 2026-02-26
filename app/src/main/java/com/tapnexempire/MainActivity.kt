a3f9f4b230efd44033219361755755f274ba7e16
idem77 <karansingh7395082@gmail.com>
2026-02-26 11:28:33 +0530

    Update MainActivity.kt

--- app/src/main/java/com/tapnexempire/MainActivity.kt
+++ app/src/main/java/com/tapnexempire/MainActivity.kt
@@ -4,24 +4,46 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.Surface
-import androidx.compose.ui.graphics.Color
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.*
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.tapnexempire.navigation.AppNavGraph
+import com.tapnexempire.navigation.BottomNavBar
+import com.tapnexempire.navigation.Routes
 import com.tapnexempire.ui.theme.TapnexEmpireTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
         setContent {
             TapnexEmpireTheme {
-                Surface(color = Color.White) {
-                    val navController = rememberNavController()
-                    AppNavGraph(navController = navController)
+
+                val navController = rememberNavController()
+                val navBackStackEntry by navController.currentBackStackEntryAsState()
+                val currentRoute = navBackStackEntry?.destination?.route
+
+                Scaffold(
+                    bottomBar = {
+                        // Hide bottom bar on detail screen
+                        if (currentRoute != "${Routes.TOURNAMENT_DETAIL}/{tournamentId}"
+                            && currentRoute != Routes.WITHDRAW
+                        ) {
+                            BottomNavBar(navController)
+                        }
+                    }
+                ) { innerPadding ->
+
+                    AppNavGraph(
+                        navController = navController
+                    )
                 }
             }
         }
