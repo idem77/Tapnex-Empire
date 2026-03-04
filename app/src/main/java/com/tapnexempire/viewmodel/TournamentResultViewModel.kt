84e11be378019b460847f6d6f9d98953228f5775
idem77 <karansingh7395082@gmail.com>
2026-03-04 14:07:28 +0530

    Rename TournamentResultViewmodel to TournamentResultViewModel

---
+++ app/src/main/java/com/tapnexempire/viewmodel/TournamentResultViewModel.kt
@@ -0,0 +1,10 @@
+package com.tapnexempire.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class TournamentResultViewModel : ViewModel() {
+
+    fun submitResult() {
+        // Will implement later
+    }
+}
