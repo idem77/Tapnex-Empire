1edbd649097325f01fd3db5d9de63a0ea24667d9
idem77 <karansingh7395082@gmail.com>
2026-03-08 20:26:58 +0530

    Update TournamentViewModel.kt

--- app/src/main/java/com/tapnexempire/viewmodel/TournamentViewModel.kt
+++ app/src/main/java/com/tapnexempire/viewmodel/TournamentViewModel.kt
@@ -2,36 +2,29 @@ package com.tapnexempire.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.tapnexempire.models.TournamentModel
-import com.tapnexempire.repository.TournamentRepository
+import com.tapnexempire.data.model.Tournament
+import com.tapnexempire.data.repository.TournamentRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class TournamentViewModel(
+@HiltViewModel
+class TournamentViewModel @Inject constructor(
     private val repository: TournamentRepository
 ) : ViewModel() {
 
-    private val _tournaments = MutableStateFlow<List<TournamentModel>>(emptyList())
-    val tournaments: StateFlow<List<TournamentModel>> = _tournaments
+    private val _tournaments = MutableStateFlow<List<Tournament>>(emptyList())
+    val tournaments: StateFlow<List<Tournament>> = _tournaments
 
     fun loadTournaments() {
         viewModelScope.launch {
-            _tournaments.value = repository.getAllTournaments()
-        }
-    }
-
-    fun joinTournament(
-        tournamentId: String,
-        userId: String,
-        entryFee: Long
-    ) {
-        viewModelScope.launch {
-            repository.joinTournament(
-                tournamentId = tournamentId,
-                userId = userId,
-                entryFee = entryFee
-            )
+            try {
+                _tournaments.value = repository.getTournaments()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
         }
     }
 }
