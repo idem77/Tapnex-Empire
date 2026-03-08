dccbe03b1bcbb4a024b7e1287347e3317d25abe4
idem77 <karansingh7395082@gmail.com>
2026-03-08 20:45:48 +0530

    Update WalletViewModel.kt

--- app/src/main/java/com/tapnexempire/viewmodel/WalletViewModel.kt
+++ app/src/main/java/com/tapnexempire/viewmodel/WalletViewModel.kt
@@ -3,7 +3,7 @@ package com.tapnexempire.viewmodel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.tapnexempire.repository.WalletRepository
-import com.tapnexempire.model.Wallet
+import com.tapnexempire.models.Wallet
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
