7270fdaa5e79907e3c97f61cc31ab1ca2bd6be68
idem77 <karansingh7395082@gmail.com>
2026-03-09 21:44:13 +0530

    Update AuthViewModel.kt

--- app/src/main/java/com/tapnexempire/viewmodel/AuthViewModel.kt
+++ app/src/main/java/com/tapnexempire/viewmodel/AuthViewModel.kt
@@ -3,7 +3,7 @@ package com.tapnexempire.viewmodel
 import android.app.Activity
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.tapnexempire.repository.AuthRepository
+import com.tapnexempire.data.repository.AuthRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
