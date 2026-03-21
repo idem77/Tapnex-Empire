f8d654ade18ab8a3b3c40777851fe541a24b0cd0
idem77 <karansingh7395082@gmail.com>
2026-03-21 12:13:13 +0530

    Create CoinUtils.kt

---
+++ app/src/main/java/com/tapnexempire/utils/CoinUtils.kt
@@ -0,0 +1,46 @@
+package.com.tapnex.empire.utils  
+  
+fun deductCoins(
+    wallet: WalletModel,
+    amount: Long
+): WalletModel {
+
+    var remaining = amount
+
+    var bonus = wallet.bonusCoins
+    var deposit = wallet.depositCoins
+    var withdrawable = wallet.withdrawableCoins
+
+    if (bonus >= remaining) {
+        bonus -= remaining
+        remaining = 0
+    } else {
+        remaining -= bonus
+        bonus = 0
+    }
+
+    if (remaining > 0) {
+        if (deposit >= remaining) {
+            deposit -= remaining
+            remaining = 0
+        } else {
+            remaining -= deposit
+            deposit = 0
+        }
+    }
+
+    if (remaining > 0) {
+        if (withdrawable >= remaining) {
+            withdrawable -= remaining
+            remaining = 0
+        } else {
+            throw Exception("Not enough balance")
+        }
+    }
+
+    return wallet.copy(
+        bonusCoins = bonus,
+        depositCoins = deposit,
+        withdrawableCoins = withdrawable
+    )
+}
