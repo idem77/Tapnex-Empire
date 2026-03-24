2cd971a8ed58617557db2155e19e29a179d8fb22
idem77 <karansingh7395082@gmail.com>
2026-03-24 20:48:43 +0530

    Update WithdrawViewModel.kt

--- app/src/main/java/com/tapnexempire/viewmodel/WithdrawViewModel.kt
+++ app/src/main/java/com/tapnexempire/viewmodel/WithdrawViewModel.kt
@@ -2,22 +2,28 @@ package com.tapnexempire.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.tapnexempire.data.repository.WalletRepository
-import dagger.hilt.android.lifecycle.HiltViewModel
+import com.tapnexempire.data.repository.WithdrawRepository
+import com.tapnexempire.utils.UiState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-@HiltViewModel
 class WithdrawViewModel @Inject constructor(
-    private val walletRepository: WalletRepository
+    private val repo: WithdrawRepository
 ) : ViewModel() {
+
+    private val _state =
+        MutableStateFlow<UiState<String>>(UiState.Loading)
+    val state: StateFlow<UiState<String>> = _state
 
     fun withdraw(userId: String, amount: Long) {
         viewModelScope.launch {
             try {
-                walletRepository.withdrawCoins(userId, amount)
+                repo.requestWithdraw(userId, amount)
+                _state.value = UiState.Success("Request Submitted")
             } catch (e: Exception) {
-                e.printStackTrace()
+                _state.value = UiState.Error(e.message ?: "Error")
             }
         }
     }
