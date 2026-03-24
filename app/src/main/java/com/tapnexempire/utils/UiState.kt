6e1184760ea76fd95b667b9e40b5d627defd222f
idem77 <karansingh7395082@gmail.com>
2026-03-24 14:02:28 +0530

    Update UiState.kt

--- app/src/main/java/com/tapnexempire/utils/UiState.kt
+++ app/src/main/java/com/tapnexempire/utils/UiState.kt
@@ -1,5 +1,7 @@
 package com.tapnexempire.utils
 
+import com.tapnexempire.utils
+
 sealed class UiState<out T> {
     object Loading : UiState<Nothing>()
     data class Success<T>(val data: T) : UiState<T>()
