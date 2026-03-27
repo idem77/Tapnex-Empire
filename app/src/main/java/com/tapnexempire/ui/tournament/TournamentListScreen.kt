e5cb0f23d968d4323fb1c4f65bb19a18ef03e5b1
idem77 <karansingh7395082@gmail.com>
2026-03-27 12:29:46 +0530

    Update TournamentListScreen.kt

--- app/src/main/java/com/tapnexempire/ui/tournament/TournamentListScreen.kt
+++ app/src/main/java/com/tapnexempire/ui/tournament/TournamentListScreen.kt
@@ -2,7 +2,8 @@ package com.tapnexempire.ui.tournament
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.items   
+import androidx.compose.ui.Alignment  
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
