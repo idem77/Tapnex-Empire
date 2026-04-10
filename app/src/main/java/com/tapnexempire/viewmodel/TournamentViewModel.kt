5ee5b553664ecf2059bcbb173e8255c962fbfd8c
idem77 <karansingh7395082@gmail.com>
2026-04-10 14:52:47 +0530

    Update TournamentViewModel.kt

--- app/src/main/java/com/tapnexempire/viewmodel/TournamentViewModel.kt
+++ app/src/main/java/com/tapnexempire/viewmodel/TournamentViewModel.kt
@@ -1,13 +1,8 @@
 package com.tapnexempire.viewmodel
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.tapnexempire.data.model.TournamentModel
 import com.tapnexempire.data.repository.TournamentRepository
-import com.tapnexempire.utils.UiState
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.*
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -15,23 +10,12 @@ class TournamentViewModel @Inject constructor(
     private val repo: TournamentRepository
 ) : ViewModel() {
 
-    private val _tournaments =
-        MutableStateFlow<List<TournamentModel>>(emptyList())
-    val tournaments: StateFlow<List<TournamentModel>> = _tournaments
-
-    fun startListening() {
-        repo.listenToTournaments {
-            _tournaments.value = it
-        }
-    }
-
     fun joinTournament(
         tournamentId: String,
         userId: String,
-        entryFee: Long
+        entryFee: Long,
+        onResult: (Boolean, String) -> Unit
     ) {
-        viewModelScope.launch {
-            repo.joinTournament(tournamentId, userId, entryFee)
-        }
+        repo.joinTournament(tournamentId, userId, entryFee, onResult)
     }
 }
