4f8bc35b9f9db7314042774ac9143d3aaf2788d1
idem77 <karansingh7395082@gmail.com>
2026-04-20 10:27:31 +0530

    Update OTPLoginScreen.kt

--- app/src/main/java/com/tapnexempire/ui/auth/OTPLoginScreen.kt
+++ app/src/main/java/com/tapnexempire/ui/auth/OTPLoginScreen.kt
@@ -1,26 +1,18 @@
 package com.tapnexempire.ui.auth
 
-import android.app.Activity
-import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
-import com.tapnexempire.viewmodel.AuthViewModel
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
 
 @Composable
 fun OTPLoginScreen(navController: NavHostController) {
 
-    // जब OTP send हो जाए
-    val verificationId = "dummy_verification" // (firebase से आएगा बाद में)
-
     Button(onClick = {
+
+        val verificationId = "test123" // firebase बाद में देगा
+
         navController.navigate("otp_verify/$verificationId")
+
     }) {
         Text("Send OTP")
     }
