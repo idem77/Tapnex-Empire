dfea3fdee3389125ccc0b2a3acc95869145d710e
idem77 <karansingh7395082@gmail.com>
2026-05-08 15:17:22 +0530

    Update TransactionModel.kt

--- app/src/main/java/com/tapnexempire/data/model/TransactionModel.kt
+++ app/src/main/java/com/tapnexempire/data/model/TransactionModel.kt
@@ -9,10 +9,20 @@ enum class TransactionType {
 }
 
 data class TransactionModel(
+
     val id: String = "",
+
     val userId: String = "",
+
     val type: TransactionType = TransactionType.BONUS,
+
     val amount: Long = 0,
+
+    val coins: Long = 0,
+
+    val status: String = "SUCCESS",
+
     val description: String = "",
+
     val createdAt: Long = System.currentTimeMillis()
 )
