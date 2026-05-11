cb4a7c9c0737646797f2b5fde60bf9591c0bfcbe
idem77 <karansingh7395082@gmail.com>
2026-05-11 15:02:30 +0530

    Update TransactionHistoryScreen.kt

--- app/src/main/java/com/tapnexempire/ui/wallet/TransactionHistoryScreen.kt
+++ app/src/main/java/com/tapnexempire/ui/wallet/TransactionHistoryScreen.kt
@@ -49,7 +49,7 @@ fun TransactionHistoryScreen(
                 ) {
 
                     Text(
-                        text = transaction.type.name,
+                        text = transaction.type.
                         style = MaterialTheme.typography.titleMedium
                     )
 
