da1b7013cbe07c524178359fcc491cf1f8497dec
idem77 <karansingh7395082@gmail.com>
2026-05-21 13:40:19 +0530

    Create BundelModel.kt

---
+++ app/src/main/java/com/tapnexempire/data/model/BundelModel.kt
@@ -0,0 +1,10 @@
+package com.tapnexempire.data.model 
+
+data class BundleModel(
+
+    val id: String = "",
+    val name: String = "",
+    val rarity: String = "",
+    val characterImage: String = "",
+    val auraEffect: String = ""
+)
