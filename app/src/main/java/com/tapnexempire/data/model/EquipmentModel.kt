22f1e35fec4d38fed98da35fd494e4be71fa9e7d
idem77 <karansingh7395082@gmail.com>
2026-05-21 13:41:13 +0530

    Add EquipmentModel data class with properties

---
+++ app/src/main/java/com/tapnexempire/data/model/EquipmentModel.kt
@@ -0,0 +1,9 @@
+package com.tapnexempire.data.model
+
+ data class EquipmentModel(
+
+    val weapon: String = "",
+    val outfit: String = "",
+    val wings: String = "",
+    val aura: String = ""
+) 
