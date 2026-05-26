23b7cd2bcabdd609971e848b5e000068d4651a30
idem77 <karansingh7395082@gmail.com>
2026-05-26 11:39:03 +0530

    Update DepositRequestScreen.kt

--- app/src/main/java/com/tapnexempire/ui/admin/DepositRequestScreen.kt
+++ app/src/main/java/com/tapnexempire/ui/admin/DepositRequestScreen.kt
@@ -1,1 +1,12 @@
+package com.tapnexempire.ui.admin
 
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun DepositRequestScreen() {
+
+    Text(
+        text = "Deposit Requests"
+    )
+}
