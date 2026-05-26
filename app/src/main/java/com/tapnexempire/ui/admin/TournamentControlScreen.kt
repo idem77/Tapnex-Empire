6fc11d5807b07173f21d75f7d1d3e26f69d0ef05
idem77 <karansingh7395082@gmail.com>
2026-05-26 20:58:37 +0530

    Update TournamentControlScreen.kt

--- app/src/main/java/com/tapnexempire/ui/admin/TournamentControlScreen.kt
+++ app/src/main/java/com/tapnexempire/ui/admin/TournamentControlScreen.kt
@@ -1,12 +1,1 @@
-package com.tapnexempire.ui.admin
 
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-
-@Composable
-fun TournamentControlScreen() {
-
-    Text(
-        text = "Tournament Control"
-    )
-}
