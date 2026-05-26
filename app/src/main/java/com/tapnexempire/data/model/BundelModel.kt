c871c6b012712abd3a8b64fd742e067d5bde8b40
idem77 <karansingh7395082@gmail.com>
2026-05-26 12:59:16 +0530

    Update BundelModel.kt

--- app/src/main/java/com/tapnexempire/data/model/BundelModel.kt
+++ app/src/main/java/com/tapnexempire/data/model/BundelModel.kt
@@ -1,10 +1,24 @@
-package com.tapnexempire.data.model 
+package com.tapnexempire.data.model
 
 data class BundleModel(
 
     val id: String = "",
-    val name: String = "",
+
+    val title: String = "",
+
     val rarity: String = "",
-    val characterImage: String = "",
-    val auraEffect: String = ""
+
+    val price: Int = 0,
+
+    val previewImage: String = "",
+
+    val auraType: String = "",
+
+    val weaponType: String = "",
+
+    val wingsType: String = "",
+
+    val isLimited: Boolean = false,
+
+    val isActive: Boolean = true
 )
