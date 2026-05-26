f1823cce2d2c02361b09d2bbc7abfc8117cbeb12
idem77 <karansingh7395082@gmail.com>
2026-05-26 11:38:26 +0530

    Update WithdrawRequestScreen.kt

--- app/src/main/java/com/tapnexempire/ui/admin/WithdrawRequestScreen.kt
+++ app/src/main/java/com/tapnexempire/ui/admin/WithdrawRequestScreen.kt
@@ -1,1 +1,12 @@
+package com.tapnexempire.ui.admin
 
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun WithdrawRequestScreen() {
+
+    Text(
+        text = "Withdraw Requests"
+    )
+}
