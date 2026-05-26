3b4eef8c241b60dfdd8b09232dbd0d6f1fa2f24d
idem77 <karansingh7395082@gmail.com>
2026-05-26 20:57:13 +0530

    Update EventControlScreen.kt

--- app/src/main/java/com/tapnexempire/ui/admin/EventControlScreen.kt
+++ app/src/main/java/com/tapnexempire/ui/admin/EventControlScreen.kt
@@ -1,12 +1,1 @@
-package com.tapnexempire.ui.admin
 
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-
-@Composable
-fun EventControlScreen() {
-
-    Text(
-        text = "Event Control"
-    )
-}
