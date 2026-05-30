52e446e3bd6908541e3c41b9f5bf5463cf998c6f
idem77 <karansingh7395082@gmail.com>
2026-05-30 10:50:07 +0530

    Update CharacterModel.kt

--- app/src/main/java/com/tapnexempire/data/model/CharacterModel.kt
+++ app/src/main/java/com/tapnexempire/data/model/CharacterModel.kt
@@ -2,27 +2,20 @@ package com.tapnexempire.data.model
 
 data class CharacterModel(
 
-val id: String = "",
+    val id: String = "",
 
-val name: String = "",
+    val name: String = "",
 
-val level: Int = 1,
+    val level: Int = 1,
 
-val rank: String = "Beginner",
+    val rank: String = "Beginner",
 
-val currentPose:
-    CharacterPose = CharacterPose.IDLE,
+    val currentPose:
+        CharacterPose = CharacterPose.IDLE,
 
-val equippedItems:
-    EquipmentModel = EquipmentModel(),
+    val selectedBundle:
+        BundleModel = BundleModel(),
 
-val selectedBundle:
-    BundleModel = BundleModel(),
-
-val selectedAura: String = "none",
-
-val selectedWings: String = "none",
-
-val selectedWeapon: String = "none"
-
+    val selectedAura: String =
+        "light_aura"
 )
