d25b451fdf034d574a7fc1858350a829019f7273
idem77 <karansingh7395082@gmail.com>
2026-06-04 09:30:11 +0530

    Create BundleData.kt

---
+++ app/src/main/java/com/tapnexempire/data/model/BundleData.kt
@@ -0,0 +1,50 @@
+package com.tapnexempire.data.model
+
+val bundles = listOf(
+
+BundleModel(
+
+    id = "shadow_reaper",
+
+    title = "Shadow Reaper",
+
+    rarity = "Legendary",
+
+    bundleImage = "bundle_shadow_reaper",
+
+    defaultRune = "void_rune",
+
+    defaultMagic = "shadow_orb"
+),
+
+BundleModel(
+
+    id = "lightbringer",
+
+    title = "Light Bringer",
+
+    rarity = "Epic",
+
+    bundleImage = "bundle_lightbringer",
+
+    defaultRune = "light_rune",
+
+    defaultMagic = "lightning_orb"
+),
+
+BundleModel(
+
+    id = "celestial_divine",
+
+    title = "Celestial Divine",
+
+    rarity = "Mythic",
+
+    bundleImage = "bundle_celestial_divine",
+
+    defaultRune = "divine_rune",
+
+    defaultMagic = "spirit_energy"
+)
+
+)
