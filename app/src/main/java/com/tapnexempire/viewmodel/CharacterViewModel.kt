9540dd11d0495c5b93110bd4c89aaf75a58ddcc1
idem77 <karansingh7395082@gmail.com>
2026-06-04 10:47:27 +0530

    Update CharacterViewModel.kt

--- app/src/main/java/com/tapnexempire/viewmodel/CharacterViewModel.kt
+++ app/src/main/java/com/tapnexempire/viewmodel/CharacterViewModel.kt
@@ -40,7 +40,7 @@ class CharacterViewModel : ViewModel() {
     characterState =
         characterState.copy(
 
-            selectedRune = aura
+            selectedRune = rune
         )
           }
 
