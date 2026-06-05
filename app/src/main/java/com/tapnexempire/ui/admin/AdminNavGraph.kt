a4dbb4fe88bc23920699395ffa49e2c24d233483
idem77 <karansingh7395082@gmail.com>
2026-06-05 12:23:35 +0530

    Update AdminNavGraph.kt

--- app/src/main/java/com/tapnexempire/ui/admin/AdminNavGraph.kt
+++ app/src/main/java/com/tapnexempire/ui/admin/AdminNavGraph.kt
@@ -1,1 +1,48 @@
+package com.tapnexempire.ui.admin
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+
+@Composable
+fun AdminNavGraph(navController: NavHostController) {
+
+    NavHost(
+        navController = navController,
+        startDestination = "admin_dashboard"
+    ) {
+
+        composable("admin_dashboard") {
+            AdminDashboardScreen(navController)
+        }
+
+        composable("user_management") {
+            UserManagementScreen()
+        }
+
+        composable("tournament_control") {
+            TournamentControlScreen()
+        }
+
+        composable("deposit_requests") {
+            DepositRequestScreen()
+        }
+
+        composable("withdraw_requests") {
+            WithdrawRequestScreen()
+        }
+
+        composable("event_control") {
+            EventControlScreen()
+        }
+
+        composable("bundle_control") {
+            BundleControlScreen()
+        }
+
+        composable("character_control") {
+            CharacterControlScreen()
+        }
+    }
+}
