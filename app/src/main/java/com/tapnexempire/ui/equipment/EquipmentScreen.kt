62600c6bf7f4340ee88ae13f2f88e586ca26d366
idem77 <karansingh7395082@gmail.com>
2026-06-05 10:46:37 +0530

    Update EquipmentScreen.kt

--- app/src/main/java/com/tapnexempire/ui/equipment/EquipmentScreen.kt
+++ app/src/main/java/com/tapnexempire/ui/equipment/EquipmentScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.foundation.background
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -104,5 +105,3 @@ Box(
         )
     }
   }
- }
-}
