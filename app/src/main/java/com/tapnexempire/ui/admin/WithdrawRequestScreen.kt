1505d9873aff50b2afc9500f403be2b1a15d3696
idem77 <karansingh7395082@gmail.com>
2026-06-06 15:36:46 +0530

    Update WithdrawRequestScreen.kt

--- app/src/main/java/com/tapnexempire/ui/admin/WithdrawRequestScreen.kt
+++ app/src/main/java/com/tapnexempire/ui/admin/WithdrawRequestScreen.kt
@@ -19,7 +19,7 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun WithdrawRequestScreen(
-    adminViewModel: AdminViewModel = hiltViewModel()
+    adminViewModel: AdminViewModel = ViewModel()
 ) {
 
     val withdraws = adminViewModel.withdrawsLive
