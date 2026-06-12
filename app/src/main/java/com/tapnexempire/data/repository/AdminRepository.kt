e53d83c8346ea09991ed328dcc4766934da40ebf
idem77 <karansingh7395082@gmail.com>
2026-06-12 12:33:13 +0530

    Update AdminRepository.kt

--- app/src/main/java/com/tapnexempire/data/repository/AdminRepository.kt
+++ app/src/main/java/com/tapnexempire/data/repository/AdminRepository.kt
@@ -35,8 +35,8 @@ class AdminRepository {
         AdminLiveRepository.rejectWithdraw(withdrawId)
 
     // 🏆 TOURNAMENT
-    fun createTournament(title: String, entryFee: Long, prizePool: Long) =
-        AdminLiveRepository.createTournament(title, entryFee, prizePool)
+    fun createTournament(title: String, entryFee: Long, prizePool: Long, maxPlayers: Long ) =
+        AdminLiveRepository.createTournament(title, entryFee, prizePool, maxPlayers)
 
     fun closeTournament(id: String) =
         AdminLiveRepository.closeTournament(id)
