a955c9367843eee0f7ee47a221bc0e0cb80d3e4d
idem77 <karansingh7395082@gmail.com>
2026-06-19 06:24:05 +0530

    Update and rename MyTournamentScreen.kt to MyTournamentModel.kt

---
+++ app/src/main/java/com/tapnexempire/data/model/MyTournamentModel.kt
@@ -0,0 +1,14 @@
+package com.tapnexempire.data.model
+
+data class MyTournamentModel(
+
+    val tournamentId: String = "",
+
+    val title: String = "",
+
+    val status: String = "",
+
+    val score: Long = 0,
+
+    val rank: Long = 0
+)
