ce4b08e9c370d561188b5adce20e3a054c386230
idem77 <karansingh7395082@gmail.com>
2026-06-21 13:12:37 +0530

    Update EmpireWalletCard.kt

--- app/src/main/java/com/tapnexempire/components/EmpireWalletCard.kt
+++ app/src/main/java/com/tapnexempire/components/EmpireWalletCard.kt
@@ -9,6 +9,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.foundation.Image
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.foundation.background
