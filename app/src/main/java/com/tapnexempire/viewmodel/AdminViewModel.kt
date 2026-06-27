34e7a5616b93ef51a5f0669edace45630d7db07b
idem77 <karansingh7395082@gmail.com>
2026-06-27 13:27:21 +0530

    Update AdminViewModel.kt

--- app/src/main/java/com/tapnexempire/viewmodel/AdminViewModel.kt
+++ app/src/main/java/com/tapnexempire/viewmodel/AdminViewModel.kt
@@ -30,8 +30,8 @@ class AdminViewModel : ViewModel()  {
     fun listenWithdraws(onUpdate: (List<Map<String, Any>>) -> Unit) =
         AdminLiveRepository.listenWithdraws(onUpdate)
 
-    fun approveWithdraw(userId: String, withdrawId: String, amount: Long) =
-        AdminLiveRepository.approveWithdraw(userId, withdrawId, amount)
+    fun approveWithdraw(userId: String, withdrawId: String, amount: Long, redeemCode: String) =
+        AdminLiveRepository.approveWithdraw(userId, withdrawId, amount, redeemCode)
 
     fun rejectWithdraw(withdrawId: String) =
         AdminLiveRepository.rejectWithdraw(withdrawId)
