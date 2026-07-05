8774b47ec7906d587adea215cdb702ec43e57944
idem77 <karansingh7395082@gmail.com>
2026-07-05 15:19:48 +0530

    Update Color.kt

--- app/src/main/java/com/tapnexempire/ui/theme/Color.kt
+++ app/src/main/java/com/tapnexempire/ui/theme/Color.kt
@@ -19,10 +19,16 @@ val EmpireCard = Color(0xFF1D2128)
 val EmpireWhite = Color(0xFFF5F5F5)
 val EmpireGray = Color(0xFF9E9E9E)
 
+
 // ACCENTS
 val EmpireBlue = Color(0xFF64B5F6)
 val EmpireGreen = Color(0xFF81C784)
 val EmpireRed = Color(0xFFE57373)
+
+// ROYAL OCEAN BUTTONS
+val EmpireButtonBlue = Color(0xFF1E88E5)
+val EmpireButtonBlueDark = Color(0xFF1565C0)
+val EmpireButtonText = Color.White
 
 // OLD SUPPORT COLORS
 val PinkPeachLight = Color(0xFFFFE0E0)
@@ -40,6 +46,8 @@ val SoftGray = EmpireGray
 
 val CardBackground = EmpireCard
 
-val ButtonBackground = EmpireGold
+val ButtonBackground = EmpireButtonBlue
 
-val ButtonTextColor = Color.Black
+val ButtonTextColor = EmpireButtonText
+
+
