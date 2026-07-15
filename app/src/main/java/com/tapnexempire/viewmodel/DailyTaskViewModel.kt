a69cd48eae9db8d9da43c47454f38b97bb880f7a
idem77 <karansingh7395082@gmail.com>
2026-07-15 13:36:22 +0530

    Create DailyTaskViewModel.kt

---
+++ app/src/main/java/com/tapnexempire/viewmodel/DailyTaskViewModel.kt
@@ -0,0 +1,60 @@
+package com.tapnexempire.presentation.task
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.tapnexempire.data.model.DailyTaskModel
+import com.tapnexempire.data.repository.DailyTaskRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+
+@HiltViewModel
+class DailyTaskViewModel @Inject constructor(
+    private val repository: DailyTaskRepository
+) : ViewModel() {
+
+
+    private val _tasks =
+        MutableStateFlow<List<DailyTaskModel>>(
+            emptyList()
+        )
+
+    val tasks: StateFlow<List<DailyTaskModel>> =
+        _tasks
+
+
+
+    fun loadTasks(){
+
+        viewModelScope.launch {
+
+            _tasks.value =
+                repository.getDailyTasks()
+
+        }
+
+    }
+
+
+
+    fun checkAnswer(
+        userAnswer: String,
+        correctAnswer: String
+    ): Boolean {
+
+
+        return userAnswer
+            .trim()
+            .lowercase()
+            ==
+            correctAnswer
+                .trim()
+                .lowercase()
+
+    }
+
+
+}
